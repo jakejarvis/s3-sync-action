4abfae443956832428f0f70df7d68544751a0db6
Jake Jarvis <jakejarvis@gmail.com>
2019-08-22 10:29:28 -0400

    fix shell file?

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
@@ -8,7 +8,6 @@ touch ~/.aws/credentials
 echo "[default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
-
 
 aws s3 sync ${SOURCE_DIR} s3://${AWS_S3_BUCKET} \
             --follow-symlinks \
