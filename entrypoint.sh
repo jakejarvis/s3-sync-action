49a6035a0a56aa8095a5867642d39d60d5a24cc3
Jake Jarvis <jake@jarv.is>
2019-09-05 07:53:03 -0400

    Allow custom arguments (#2)

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,6 @@ aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 aws s3 sync ${SOURCE_DIR} s3://${AWS_S3_BUCKET} \
             --follow-symlinks \
             --delete \
-            --region ${AWS_REGION} \
-            --metadata-directive "REPLACE"
+            --region ${AWS_REGION} $*
 
 rm -rf ~/.aws
