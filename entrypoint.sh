4ca84f590a8c60ec4542a970637be0bba0d94f6a
Jake Jarvis <jake@jarv.is>
2019-09-12 12:55:26 -0400

    Check for required environment variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,31 @@
 
 set -e
 
+if [ -z "$AWS_S3_BUCKET" ]; then
+  echo "AWS_S3_BUCKET is not set. Quitting."
+  exit 1
+fi
+
+if [ -z "$AWS_ACCESS_KEY_ID" ]; then
+  echo "AWS_ACCESS_KEY_ID is not set. Quitting."
+  exit 1
+fi
+
+if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+  echo "AWS_SECRET_ACCESS_KEY is not set. Quitting."
+  exit 1
+fi
+
+if [ -z "$AWS_REGION" ]; then
+  echo "AWS_REGION is not set. Quitting."
+  exit 1
+fi
+
+if [ -z "$SOURCE_DIR" ]; then
+  echo "SOURCE_DIR is not set. Quitting."
+  exit 1
+fi
+
 mkdir -p ~/.aws
 touch ~/.aws/credentials
 
