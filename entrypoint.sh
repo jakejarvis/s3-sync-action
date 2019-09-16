852d7b1daedc2f0997cb94c016d461fda4f99c49
Jake Jarvis <jake@jarv.is>
2019-09-16 13:56:23 -0400

    Silence `aws configure` prompt

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,7 +30,7 @@ fi
 # Create a dedicated profile for this action to avoid
 # conflicts with other actions.
 # https://github.com/jakejarvis/s3-sync-action/issues/1
-aws configure --profile s3-sync-action <<-EOF
+aws configure --profile s3-sync-action <<-EOF > /dev/null 2>&1
 ${AWS_ACCESS_KEY_ID}
 ${AWS_SECRET_ACCESS_KEY}
 ${AWS_REGION}
