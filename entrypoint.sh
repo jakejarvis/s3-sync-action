301a060a8736b02f398299734a6a7afa7a36b6d5
Justin <ujkkju117@outlook.com>
2021-01-12 16:13:24 +1030

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,16 +7,6 @@ if [ -z "$AWS_S3_BUCKET" ]; then
   exit 1
 fi
 
-# if [ -z "$AWS_ACCESS_KEY_ID" ]; then
-#   echo "AWS_ACCESS_KEY_ID is not set. Quitting."
-#   exit 1
-# fi
-
-# if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
-#   echo "AWS_SECRET_ACCESS_KEY is not set. Quitting."
-#   exit 1
-# fi
-
 # Default to us-east-1 if AWS_REGION not set.
 if [ -z "$AWS_REGION" ]; then
   AWS_REGION="us-east-1"
@@ -26,13 +16,6 @@ fi
 if [ -n "$AWS_S3_ENDPOINT" ]; then
   ENDPOINT_APPEND="--endpoint-url $AWS_S3_ENDPOINT"
 fi
-
-# aws configure --profile s3-sync-action <<-EOF > /dev/null 2>&1
-# ${AWS_ACCESS_KEY_ID}
-# ${AWS_SECRET_ACCESS_KEY}
-# ${AWS_REGION}
-# text
-# EOF
 
 # Sync using our dedicated profile and suppress verbose messages.
 # All other flags are optional via the `args:` directive.
