1f3bfd525464c3c946bc2e1ba9f64cf5f6a4ea1d
Justin <ujkkju117@outlook.com>
2021-01-12 16:06:49 +1030

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,9 +27,6 @@ if [ -n "$AWS_S3_ENDPOINT" ]; then
   ENDPOINT_APPEND="--endpoint-url $AWS_S3_ENDPOINT"
 fi
 
-# Create a dedicated profile for this action to avoid conflicts
-# with past/future actions.
-# https://github.com/jakejarvis/s3-sync-action/issues/1
 # aws configure --profile s3-sync-action <<-EOF > /dev/null 2>&1
 # ${AWS_ACCESS_KEY_ID}
 # ${AWS_SECRET_ACCESS_KEY}
@@ -37,48 +34,8 @@ fi
 # text
 # EOF
 
-# Assume Role if user sets AWS_ASSUMED_ROLE.
-# if [ -n "$AWS_ASSUMED_ROLE" ]; then
-#   role_arn="$AWS_ASSUMED_ROLE"
-  
-#   export AWS_PROFILE=${PROFILE}
-  
-#   JSON=$(aws sts assume-role \
-#           --role-arn "$AWS_ASSUMED_ROLE" \
-#           --role-session-name "S3 Update CI" \
-#           --duration-seconds 600
-#           2>/dev/null) || { echo "Error assuming role"; exit 1; }
-#   echo ${JSON}
-#   AWS_ACCESS_KEY_ID=$(echo ${JSON} | jq --raw-output ".Credentials[\"AccessKeyId\"]")
-#   AWS_SECRET_ACCESS_KEY=$(echo ${JSON} | jq --raw-output ".Credentials[\"SecretAccessKey\"]")
-#   AWS_SESSION_TOKEN=$(echo ${JSON} | jq --raw-output ".Credentials[\"SessionToken\"]")
-#   AWS_EXPIRATION=$(echo ${JSON} | jq --raw-output ".Credentials[\"Expiration\"]")
-  
-#   unset AWS_PROFILE
-  
-#   export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
-#   export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
-#   export AWS_SESSION_TOKEN=${AWS_SESSION_TOKEN}
-#   export AWS_ACCOUNT=${aws_account}
-#   export AWS_ACCOUNT_ID=${ACCOUNT}
-  
-#   # SaveCredentials
-# fi
-
 # Sync using our dedicated profile and suppress verbose messages.
 # All other flags are optional via the `args:` directive.
 sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
-              --profile s3-sync-action \
               --no-progress \
               ${ENDPOINT_APPEND} $*"
-
-# Clear out credentials after we're done.
-# We need to re-run `aws configure` with bogus input instead of
-# deleting ~/.aws in case there are other credentials living there.
-# https://forums.aws.amazon.com/thread.jspa?threadID=148833
-aws configure --profile s3-sync-action <<-EOF > /dev/null 2>&1
-null
-null
-null
-text
-EOF
